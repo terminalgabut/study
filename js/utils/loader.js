c98bee5c2440bdadb0420511b30243338a8a1081
terminalgabut <trasipahit02@gmail.com>
2026-02-01 23:05:36 +0700

    Update loader.js

--- js/utils/loader.js
+++ js/utils/loader.js
@@ -1,4 +1,6 @@
 export async function load(path) {
-  const res = await fetch(path);
+  const url = new URL(path, import.meta.url);
+  const res = await fetch(url);
+  if (!res.ok) throw new Error('Load failed');
   return res.text();
 }
