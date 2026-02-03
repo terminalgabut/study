03fcb75f6e297341625a004869ee7bc61b8424b9
terminalgabut <trasipahit02@gmail.com>
2026-02-04 01:16:25 +0700

    Update kontenBabView.js

--- pages/kontenBabView.js
+++ pages/kontenBabView.js
@@ -1,7 +1,11 @@
-// pages/kontenBabView.js
 export const kontenBabView = `
 <section class="learning">
+  <header class="learning-header">
+    <h1 class="learning-title" id="learningTitle"></h1>
+  </header>
+
   <article class="learning-content" id="learningContent">
+    
   </article>
 </section>
 `;
