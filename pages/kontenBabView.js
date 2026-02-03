1f178ad77cedf7c313ee3fea909cbc6cf075be97
terminalgabut <trasipahit02@gmail.com>
2026-02-03 23:33:44 +0700

    Create kontenBabView.js

---
+++ pages/kontenBabView.js
@@ -0,0 +1,11 @@
+// pages/kontenBabView.js
+export const kontenBabView = `
+<section class="konten-bab">
+  <h2 class="konten-title" id="kontenTitle"></h2>
+  <div class="konten-meta" id="kontenMeta"></div>
+
+  <article class="konten-body" id="kontenBody">
+    <p>Loading materi...</p>
+  </article>
+</section>
+`;
