bad587e78074cf28531656507fb398a9bc61d4ce
terminalgabut <trasipahit02@gmail.com>
2026-02-04 01:04:24 +0700

    Update kontenBabView.js

--- pages/kontenBabView.js
+++ pages/kontenBabView.js
@@ -1,11 +1,8 @@
 // pages/kontenBabView.js
 export const kontenBabView = `
-<section class="konten-bab">
-  <h2 class="konten-title" id="kontenTitle"></h2>
-  <div class="konten-meta" id="kontenMeta"></div>
-
-  <article class="konten-body" id="kontenBody">
-    <p>Loading materi...</p>
+<section class="learning">
+  <article class="learning-content" id="learningContent">
+    <!-- HTML dari DB masuk di sini -->
   </article>
 </section>
 `;
