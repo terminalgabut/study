5d7504ced5b755123b566d547f0a16b80b45acc3
terminalgabut <trasipahit02@gmail.com>
2026-02-04 02:29:28 +0700

    Update kontenBabView.js

--- pages/kontenBabView.js
+++ pages/kontenBabView.js
@@ -5,11 +5,10 @@ export const kontenBabView = `
     <div class="konten-header">
       <h1 id="learningTitle" class="konten-title"></h1>
 
-      <!-- Bookmark -->
-      <button id="bookmarkBtn" class="bookmark-btn" title="Bookmark materi">
-        <svg class="icon" xmlns="http://www.w3.org/2000/svg"
-          width="20" height="20" fill="none"
-          stroke="currentColor" stroke-width="2"
+      <!-- bookmark -->
+      <button id="bookmarkBtn" class="bookmark-btn" title="Simpan bookmark">
+        <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+          fill="none" stroke="currentColor" stroke-width="2"
           stroke-linecap="round" stroke-linejoin="round"
           viewBox="0 0 24 24">
           <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"/>
