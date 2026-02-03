d9c16fe0caf62f10b73111ee116b13cc6b406262
terminalgabut <trasipahit02@gmail.com>
2026-02-03 22:30:35 +0700

    Update babView.js

--- pages/babView.js
+++ pages/babView.js
@@ -1,6 +1,14 @@
 export const babView = `
-  <section class="bab-page">
-    <h2 class="bab-title"></h2>
-    <div id="babList" class="bab-list"></div>
-  </section>
+<section class="bab-page">
+  <div class="hero">
+    <h2 class="bab-title">📖 Daftar Bab</h2>
+    <p class="desc">
+      Pilih bab untuk mulai mempelajari materi secara bertahap
+    </p>
+  </div>
+
+  <div id="babList" class="bab-list">
+    <!-- daftar bab diisi dinamis -->
+  </div>
+</section>
 `;
