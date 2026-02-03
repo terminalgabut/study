86ec039bc73513d75d49513e3d4d924dd67d6763
terminalgabut <trasipahit02@gmail.com>
2026-02-03 20:50:46 +0700

    Update bab.js

--- js/ui/bab.js
+++ js/ui/bab.js
@@ -1,14 +1,26 @@
 import { supabase } from '../services/supabase.js';
 
-export async function initBab(category) {
-  const title = document.querySelector('.bab-title');
+export async function initBab(categoryFromUrl) {
   const list = document.getElementById('babList');
+  if (!list) return;
 
-  if (!title || !list) return;
+  const { data, error } = await supabase
+    .from('materi')
+    .select('*');
 
-  title.textContent = `Materi ${category}`;
+  if (error) {
+    list.innerHTML = '<p>Gagal load bab</p>';
+    return;
+  }
 
-  // nanti:
-  // query supabase
-  // render <a href="#materi/.../...">
+  const filtered = data.filter(row =>
+    row.category.toLowerCase().startsWith(categoryFromUrl)
+  );
+
+  list.innerHTML = filtered.map(bab => `
+    <a href="#materi/${categoryFromUrl}/${bab.slug}" class="bab-item">
+      <h4>${bab.category}</h4>
+      <p>${bab.title}</p>
+    </a>
+  `).join('');
 }
