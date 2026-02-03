176904acb21b1324020d9aeb0e11172c957c471a
terminalgabut <trasipahit02@gmail.com>
2026-02-03 15:57:32 +0700

    Update supabase.js

--- js/services/supabase.js
+++ js/services/supabase.js
@@ -1,9 +1,10 @@
-// supabase.js
-import { createClient } from '@supabase/supabase-js'
+// services/supabase.js
+// Import Supabase ESM langsung dari CDN
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-// 🔑 Ganti dengan info Supabase project-mu
-const SUPABASE_URL = 'https://gmmhsbmlqbomtrhdoxxo.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_r0gMojctdN1aftj_PVuhAQ_R0s__keH'
+// Konfigurasi Supabase
+const SUPABASE_URL = 'https://gmmhsbmlqbomtrhdoxxo.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_r0gMojctdN1aftj_PVuhAQ_R0s__keH';
 
-// buat client Supabase
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+// Buat client Supabase
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
