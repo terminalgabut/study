ed4891610acf207f268a6d89765fda5a6965f474
terminalgabut <trasipahit02@gmail.com>
2026-02-03 09:27:34 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ import { load } from './utils/loader.js';
 import { initRouter } from './router/hashRouter.js';
 import { initSidebar } from './ui/sidebar.js';
 import { initHeader } from './ui/header.js';
-import { initSettingsModal } from './ui/settings.js';
+import { initSettingsModal } from './ui/settingsModal.js';
 
 async function init() {
   const app = document.getElementById('app');
