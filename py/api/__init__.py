6b61a19c5324c074fd89ffb00fa3e0bcb9119eea
terminalgabut <trasipahit02@gmail.com>
2026-02-04 17:30:18 +0700

    Update __init__.py

--- py/api/__init__.py
+++ py/api/__init__.py
@@ -1,1 +1,8 @@
+# api/__init__.py
+
 from .client import call_ai
+from .generator import generate_quiz
+from .prompts import BASE_SYSTEM_PROMPT
+
+# Dengan ini, kamu bisa memanggil 'from api import call_ai' 
+# dari folder manapun di luar folder api.
