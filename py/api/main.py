82992230d47108a9f305e92b2b71f894f92543de
terminalgabut <trasipahit02@gmail.com>
2026-02-04 17:44:14 +0700

    Update main.py

--- py/api/main.py
+++ py/api/main.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
