7b4d31751e8361ca526129e451e8bf3246066669
terminalgabut <trasipahit02@gmail.com>
2026-02-05 21:02:33 +0700

    Update bookmark.js

--- js/ui/bookmark.js
+++ js/ui/bookmark.js
@@ -87,7 +87,7 @@ export async function initBookmarkPage() {
                   stroke-linecap="round" 
                   stroke-linejoin="round">
                    <path d="M3 6h18M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path>
-                 </svg>
+                  </svg>
           </button>
         </div>
       </div>
