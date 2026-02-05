656d391bf1528a401add04be828d72d6ab57093d
terminalgabut <trasipahit02@gmail.com>
2026-02-05 16:21:37 +0700

    Update kontenBabView.js

--- pages/kontenBabView.js
+++ pages/kontenBabView.js
@@ -4,24 +4,36 @@ export const kontenBabView = `
 
     <div class="konten-header">
       <h1 id="learningTitle" class="konten-title"></h1>
-      <button id="bookmarkBtn" class="bookmark-btn">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+
+      <button id="bookmarkBtn"
+              class="bookmark-btn"
+              title="Simpan bookmark"
+              aria-label="Bookmark materi">
+        <svg xmlns="http://www.w3.org/2000/svg"
+             width="24" height="24"
+             fill="none"
+             stroke="currentColor"
+             stroke-width="2"
+             stroke-linecap="round"
+             stroke-linejoin="round"
+             viewBox="0 0 24 24">
           <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"/>
         </svg>
       </button>
     </div>
 
     <div id="materiContainer">
-  <article id="learningContent" class="konten-isi"></article>
-  <div class="konten-actions">
-    <button id="generateQuizBtn" class="primary-btn">Latihan Soal</button>
-  </div>
-</div>
+      <article id="learningContent" class="konten-isi"></article>
+
+      <div class="konten-actions">
+        <button class="primary-btn" id="generateQuizBtn">
+          Latihan Soal
+        </button>
+      </div>
+    </div>
 
     <section class="quiz-section" id="quizSection" hidden>
-      <div id="quiz-ui" class="quiz-container">
-        </div>
-    </section>
+      </section>
 
   </div>
 </section>
