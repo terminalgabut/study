f6184e0f33794f7c7f62fdbbaf1402e808847b40
terminalgabut <trasipahit02@gmail.com>
2026-02-07 00:23:20 +0700

    Update notesView.js

--- pages/notesView.js
+++ pages/notesView.js
@@ -1,10 +1,13 @@
 export const notesView = `
-<section class="notes">
-  <div class="home-card hero"> <h1>📔 Buku Catatan</h1>
-    <p class="desc">Kumpulan poin penting yang telah kamu simpan selama belajar.</p>
+<section class="home notes-page">
+  <div class="home-card hero">
+    <h2>📔 Buku Catatan</h2>
+    <p class="desc">
+      Kumpulan poin penting yang telah kamu simpan.
+    </p>
   </div>
 
-  <div id="notesList" class="home-grid"> 
+  <div id="notesList" class="home-grid">
     <p class="loading">Memuat catatan...</p>
   </div>
 </section>
