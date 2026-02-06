448e395a0b785642cea26ed64f6b89f1ab6967a0
terminalgabut <trasipahit02@gmail.com>
2026-02-06 22:42:29 +0700

    Create notesView.js

---
+++ pages/notesView.js
@@ -0,0 +1,12 @@
+export const notesView = `
+  <div class="container">
+    <header class="page-header">
+      <h2>📔 Buku Catatan</h2>
+      <p>Kumpulan poin penting yang telah kamu simpan.</p>
+    </header>
+
+    <div id="notesList" class="notes-grid">
+      <p class="loading">Memuat catatan...</p>
+    </div>
+  </div>
+`;
