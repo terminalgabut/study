9b113518eddee5dfead7f1fd642c821eb00767df
terminalgabut <trasipahit02@gmail.com>
2026-02-13 16:04:51 +0700

    Update sidebarView.js

--- components/sidebarView.js
+++ components/sidebarView.js
@@ -60,7 +60,7 @@ export const sidebarView = `
       <path d="M3 18v-6a9 9 0 0 1 18 0v6"></path>
       <path d="M21 19a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3zM3 19a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2H3z"></path>
     </svg>
-    <span class="label">Audio Library</span>
+    <span class="label">Audio</span>
 </button>
 
   <button class="nav-btn" data-page="about">
