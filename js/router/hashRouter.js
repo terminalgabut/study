db7a538242fb2c049db3b070a50b596caadd3ffd
terminalgabut <trasipahit02@gmail.com>
2026-02-13 16:09:14 +0700

    Update hashRouter.js

--- js/router/hashRouter.js
+++ js/router/hashRouter.js
@@ -31,6 +31,7 @@ import { initHistoryPage } from '../ui/riwayat.js';
 import { initNotesList } from '../ui/notes.js';
 import { initNoteDetail } from '../ui/noteDetails.js'; 
 import { performaController } from '../controllers/performaController.js';
+import { auioController } from '../controllers/audioController.js';
 
 // js/router/hashRouter.js
 window.__DEBUG__?.log('hashRouter loaded');
