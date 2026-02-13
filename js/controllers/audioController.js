580f4d98e14e83563e5199a4ca7339b2faad23d3
terminalgabut <trasipahit02@gmail.com>
2026-02-13 22:01:03 +0700

    Update audioController.js

--- js/controllers/audioController.js
+++ js/controllers/audioController.js
@@ -73,7 +73,7 @@ export const audioController = {
                     document.getElementById('track-status').textContent = "Sedang diputar...";
                     isPlaying = true;
                     this.updateUI();
-                    islandController.show(title, 'music');
+                    
                 } else {
                     alert("Player sedang disiapkan, mohon tunggu 2 detik...");
                 }
@@ -97,7 +97,7 @@ export const audioController = {
         if (wave) wave.classList.add('music-wave-hidden');
         
         // HANYA sembunyikan island jika player benar-benar PAUSED/STOP
-        islandController.hide();
+        
     }
 }
 };
