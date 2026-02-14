5688b8b1fba26396de9b2e4d1bb0a73eda561f60
terminalgabut <trasipahit02@gmail.com>
2026-02-14 18:25:08 +0700

    Update island.js

--- js/island.js
+++ js/island.js
@@ -163,9 +163,11 @@ export const island = {
         if (this.textSpan) {
             this.textSpan.textContent = text;
         }
-    },
+    }
+
+};
+
 
     if (typeof window !== 'undefined') {
   window.island = island;
-}
-};
+    }
