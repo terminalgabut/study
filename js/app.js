9101b504a1c418789630f9a402cb61ac8a629710
terminalgabut <trasipahit02@gmail.com>
2026-02-15 21:05:11 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ import { initRouter } from './router/hashRouter.js';
 // Views
 import { headerView } from '../components/headerView.js'; 
 import { profileModalView } from '../../components/profileModalView.js'; 
-import { sidebarView } from '../components/sidebarView.js'; 
+import { sidebarView } from '../../components/sidebarView.js'; 
 
 console.log("app.js ✅");
 
