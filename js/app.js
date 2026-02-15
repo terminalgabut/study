9423cabb982a8317bce2a885437529c19e023370
terminalgabut <trasipahit02@gmail.com>
2026-02-15 19:42:39 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,12 +2,13 @@
 
 // Module
 import { supabase } from './services/supabase.js'; 
-import { initHeader, headerView } from './ui/header.js'; 
+import { initHeader } from './ui/header.js'; 
 import { initSidebar } from './ui/sidebar.js'; 
 import { audioController } from './controllers/audioController.js'; 
 import { initRouter } from './router/hashRouter.js'; 
 
 // Views
+import { sidebarView } from '../components/headerView.js'; 
 import { sidebarView } from '../components/sidebarView.js'; 
 
 console.log("app.js ✅");
