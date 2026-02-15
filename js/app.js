394b818bea226f3364c99aa0ba3d7f7ea9ff8194
terminalgabut <trasipahit02@gmail.com>
2026-02-15 07:47:37 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,22 +1,7 @@
 // root/js/app.js
 
 // Module
-import { audioController } from './controllers/audioController.js';
-import { supabase } from './services/supabase.js'; 
-import { initRouter } from './router/hashRouter.js';
-import { initSidebar } from './ui/sidebar.js';
-import { initHeader } from './ui/header.js';
-import { initSettingsModal } from './ui/settingsModal.js';
-import { initProfileModal } from './ui/auth/profileModal.js';
 
-// Views
-import { headerView } from '../components/headerView.js';
-import { sidebarView } from '../components/sidebarView.js';
-import { modalsettingsView } from '../components/modal-settingsView.js';
-import { babModalView } from '../components/babModalView.js';
-import { durasiModalView } from '../components/durasiModalView.js';
-import { ulangModalView } from '../components/ulangModalView.js';
-import { akurasiModalView } from '../components/akurasiModalView.js';
 
 alert('TEST MODULE JALAN');
 console.log("app.js ✅");
@@ -108,5 +93,4 @@ function checkLayout() {
   }
 }
 
-// 5. RUN
 init();
