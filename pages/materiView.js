bddc1a27cce99831843764c6cbdaa3652c71fc71
terminalgabut <trasipahit02@gmail.com>
2026-02-16 17:12:42 +0700

    Update materiView.js

--- pages/materiView.js
+++ pages/materiView.js
@@ -5,19 +5,19 @@ export const materiView = `
     <p class="desc">Pilih materi untuk melihat daftar bab dan pembahasan</p>
   </div>
 
-  <div class="home-card hero">
+  <div class="home">
     <!-- item materi -->
-    <a href="#/materi/bahasa" class="materi-card">
+    <a href="#/materi/bahasa" class="home-card hero">
       <h3>Struktur dasar Bahasa Indonesia</h3>
       <p class="small">Pengenalan pondasi dasar Bahasa Indonesia.</p>
     </a>
 
-    <a href="/#materi/fallacy" class="materi-card">
+    <a href="/#materi/fallacy" class="home-card hero">
       <h3>Logika Fallacy</h3>
       <p class="small">Pengenalan konsep kesalahan berpikir.</p>
     </a>
 
-    <a href="#/materi/filsafat" class="materi-card">
+    <a href="#/materi/filsafat" class="home-card">
       <h3>Filsafat dalam praktik</h3>
       <p class="small">Pengenalan konsep cara berpikir Filsafat.</p>
     </a>
