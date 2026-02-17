fa6f938d9c0e45e08ea883771dae38568fb38a71
terminalgabut <trasipahit02@gmail.com>
2026-02-17 23:37:27 +0700

    Create database.py

---
+++ py/api/database.py
@@ -0,0 +1,8 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
