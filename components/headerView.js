a4fb9698deda7ca2cd9382a14d071bd93aa8d650
terminalgabut <trasipahit02@gmail.com>
2026-02-17 07:59:02 +0700

    Update headerView.js

--- components/headerView.js
+++ components/headerView.js
@@ -10,10 +10,7 @@ export const headerView = `
   <div id="dynamic-island-container" class="island-hidden">
     <div id="dynamic-island">
       <div class="island-content">
-        <svg id="icon-music" class="island-icon" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 18V5l12-2v13"></path><circle cx="6" cy="18" r="3"></circle><circle cx="18" cy="16" r="3"></circle></svg>
-        
-        <svg id="icon-timer" class="island-icon hidden" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg>
-        
+        <span id="island-icon"></span>
         <span id="island-text"></span>
       </div>
     </div>
