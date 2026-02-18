855bd2ee792978f28ceab4d562d87c7c25ec49c0
terminalgabut <trasipahit02@gmail.com>
2026-02-18 17:27:01 +0700

    Update database.py

--- py/api/database.py
+++ py/api/database.py
@@ -1,8 +1,19 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+engine = create_engine(
+    DATABASE_URL,
+    poolclass=NullPool,     
+    pool_pre_ping=True,    
+    echo=False             
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
