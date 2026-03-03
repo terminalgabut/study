21c2ed6003c9f91aadbf0a74e18ec415288cbdfe
terminalgabut <trasipahit02@gmail.com>
2026-03-03 18:22:38 +0700

    Update sidebarView.js

--- components/sidebarView.js
+++ components/sidebarView.js
@@ -18,6 +18,40 @@ export const sidebarView = `
     <span class="label">Materi</span>
   </button>
 
+  <button class="nav-btn" data-page="catatan">
+    <svg class="icon" xmlns="http://www.w3.org/2000/svg" 
+         width="20" height="20" fill="none" stroke="currentColor"
+         stroke-width="2" stroke-linecap="round" 
+         stroke-linejoin="round" viewBox="0 0 24 24">
+      <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path>
+      <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path>
+    </svg>
+    <span class="label">Catatan</span>
+  </button>
+
+  <button class="nav-btn" data-page="performa">
+    <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-width="2"
+         stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+      <line x1="18" y1="20" x2="18" y2="10"/>
+      <line x1="12" y1="20" x2="12" y2="4"/>
+      <line x1="6" y1="20" x2="6" y2="14"/>
+    </svg>
+    <span class="label">Performa</span>
+  </button>
+
+  <button class="nav-btn" data-page="journal">
+  <svg class="icon" xmlns="http://www.w3.org/2000/svg"
+       width="20" height="20" fill="none" stroke="currentColor"
+       stroke-width="2" stroke-linecap="round"
+       stroke-linejoin="round" viewBox="0 0 24 24">
+    <path d="M2 6v14"></path>
+    <path d="M22 6v14"></path>
+    <path d="M12 6c-3-2-7-2-10 0"></path>
+    <path d="M12 6c3-2 7-2 10 0"></path>
+  </svg>
+  <span class="label">Jurnal</span>
+</button>
+
   <button class="nav-btn" data-page="riwayat">
     <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-width="2"
          stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
@@ -33,25 +67,6 @@ export const sidebarView = `
       <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"/>
     </svg>
     <span class="label">Bookmark</span>
-  </button>
-
-  <button class="nav-btn" data-page="catatan">
-    <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-width="2"
-         stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-      <path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path>
-      <path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path>
-    </svg>
-    <span class="label">Catatan</span>
-  </button>
-
-  <button class="nav-btn" data-page="performa">
-    <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-width="2"
-         stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
-      <line x1="18" y1="20" x2="18" y2="10"/>
-      <line x1="12" y1="20" x2="12" y2="4"/>
-      <line x1="6" y1="20" x2="6" y2="14"/>
-    </svg>
-    <span class="label">Performa</span>
   </button>
 
   <button class="nav-btn" data-page="audio">
